b836fce6ca708c0c107aa8357ed589ecaed6c8db
zhaolihang <625335512@qq.com>
2018-05-30 19:21:47 +0800

    格式化

--- asm.h
+++ asm.h
@@ -2,20 +2,19 @@
 // assembler macros to create x86 segments
 //
 
-#define ASM_SEG_NULL                                             \
-        .word 0, 0;                                             \
-        .byte 0, 0, 0, 0
+#define ASM_SEG_NULL \
+    .word 0, 0;      \
+    .byte 0, 0, 0, 0
 
 // The 0xC0 means the limit is in 4096-byte units
 // and (for executable segments) 32-bit mode.
-#define ASM_MAKE_SEG(type,base,lim)                                  \
-        .word (((lim) >> 12) & 0xffff), ((base) & 0xffff);      \
-        .byte (((base) >> 16) & 0xff), (0x90 | (type)),         \
-                (0xC0 | (((lim) >> 28) & 0xf)), (((base) >> 24) & 0xff)
+#define ASM_MAKE_SEG(type, base, lim)                 \
+    .word(((lim) >> 12) & 0xffff), (( base )&0xffff); \
+    .byte(((base) >> 16) & 0xff), (0x90 | (type)), (0xC0 | (((lim) >> 28) & 0xf)), (((base) >> 24) & 0xff)
 
-#define APP_SEG_TYPE_X     0x8       // Executable segment
-#define APP_SEG_TYPE_E     0x4       // Expand down (non-executable segments)
-#define APP_SEG_TYPE_C     0x4       // Conforming code segment (executable only)
-#define APP_SEG_TYPE_W     0x2       // Writeable (non-executable segments)
-#define APP_SEG_TYPE_R     0x2       // Readable (executable segments)
-#define APP_SEG_TYPE_A     0x1       // Accessed
+#define APP_SEG_TYPE_X 0x8    // Executable segment
+#define APP_SEG_TYPE_E 0x4    // Expand down (non-executable segments)
+#define APP_SEG_TYPE_C 0x4    // Conforming code segment (executable only)
+#define APP_SEG_TYPE_W 0x2    // Writeable (non-executable segments)
+#define APP_SEG_TYPE_R 0x2    // Readable (executable segments)
+#define APP_SEG_TYPE_A 0x1    // Accessed
--- bio.c
+++ bio.c
@@ -27,120 +27,108 @@
 #include "buf.h"
 
 struct {
-  struct spinlock lock;
-  struct buf buf[NBUF];
+    struct spinlock lock;
+    struct buf      buf[BUFFER_SIZE];
 
-  // Linked list of all buffers, through prev/next.
-  // head.next is most recently used.
-  struct buf head;
+    // Linked list of all buffers, through prev/next.
+    // head.next is most recently used.
+    struct buf head;
 } bcache;
 
-void
-binit(void)
-{
-  struct buf *b;
+void binit(void) {
+    struct buf* b;
 
-  initlock(&bcache.lock, "bcache");
+    initlock(&bcache.lock, "bcache");
 
-//PAGEBREAK!
-  // Create linked list of buffers  
+    //PAGEBREAK!
+    // Create linked list of buffers
 
-  // head <=> head  插入new  head <=> new <=> head
-  bcache.head.prev = &bcache.head;// 自己链接自己形成一个环 双向链表的初始化
-  bcache.head.next = &bcache.head;
-  for(b = bcache.buf; b < bcache.buf+NBUF; b++){//  在环的开头打开一个口插入一个再接上 已然还是一个环
-    b->next = bcache.head.next;
-    b->prev = &bcache.head;
-    initsleeplock(&b->lock, "buffer");
-    bcache.head.next->prev = b;
-    bcache.head.next = b;
-  }
+    // head <=> head  插入new  head <=> new <=> head
+    bcache.head.prev = &bcache.head;    // 自己链接自己形成一个环 双向链表的初始化
+    bcache.head.next = &bcache.head;
+    //  在环的开头打开一个口插入一个再接上 依然还是一个环
+    for (b = bcache.buf; b < bcache.buf + BUFFER_SIZE; b++) {
+        b->next = bcache.head.next;
+        b->prev = &bcache.head;
+        initsleeplock(&b->lock, "buffer");
+        bcache.head.next->prev = b;
+        bcache.head.next       = b;
+    }
 }
 
 // Look through buffer cache for block on device dev.
 // If not found, allocate a buffer.
 // In either case, return locked buffer.
-static struct buf*
-bget(uint dev, uint blockno)
-{
-  struct buf *b;
+static struct buf* bget(uint dev, uint blockno) {
+    struct buf* b;
 
-  acquire(&bcache.lock);
+    acquire(&bcache.lock);
 
-  // Is the block already cached?
-  for(b = bcache.head.next; b != &bcache.head; b = b->next){
-    if(b->dev == dev && b->blockno == blockno){
-      b->refcnt++;
-      release(&bcache.lock);
-      acquiresleep(&b->lock);
-      return b;
+    // Is the block already cached?
+    for (b = bcache.head.next; b != &bcache.head; b = b->next) {
+        if (b->dev == dev && b->blockno == blockno) {
+            b->refcnt++;
+            release(&bcache.lock);
+            acquiresleep(&b->lock);
+            return b;
+        }
     }
-  }
 
-  // Not cached; recycle an unused buffer.
-  // Even if refcnt==0, B_DIRTY indicates a buffer is in use
-  // because log.c has modified it but not yet committed it.
-  for(b = bcache.head.prev; b != &bcache.head; b = b->prev){
-    if(b->refcnt == 0 && (b->flags & B_DIRTY) == 0) {
-      b->dev = dev;
-      b->blockno = blockno;
-      b->flags = 0;
-      b->refcnt = 1;
-      release(&bcache.lock);
-      acquiresleep(&b->lock);
-      return b;
+    // Not cached; recycle an unused buffer.
+    // Even if refcnt==0, B_DIRTY indicates a buffer is in use
+    // because log.c has modified it but not yet committed it.
+    for (b = bcache.head.prev; b != &bcache.head; b = b->prev) {
+        if (b->refcnt == 0 && (b->flags & B_DIRTY) == 0) {
+            b->dev     = dev;
+            b->blockno = blockno;
+            b->flags   = 0;
+            b->refcnt  = 1;
+            release(&bcache.lock);
+            acquiresleep(&b->lock);
+            return b;
+        }
     }
-  }
-  panic("bget: no buffers");
+    panic("bget: no buffers");
 }
 
 // Return a locked buf with the contents of the indicated block.
-struct buf*
-bread(uint dev, uint blockno)
-{
-  struct buf *b;
+struct buf* bread(uint dev, uint blockno) {
+    struct buf* b;
 
-  b = bget(dev, blockno);
-  if((b->flags & B_VALID) == 0) {
-    iderw(b);
-  }
-  return b;
+    b = bget(dev, blockno);
+    if ((b->flags & B_VALID) == 0) {
+        iderw(b);
+    }
+    return b;
 }
 
 // Write b's contents to disk.  Must be locked.
-void
-bwrite(struct buf *b)
-{
-  if(!holdingsleep(&b->lock))
-    panic("bwrite");
-  b->flags |= B_DIRTY;
-  iderw(b);
+void bwrite(struct buf* b) {
+    if (!holdingsleep(&b->lock))
+        panic("bwrite");
+    b->flags |= B_DIRTY;
+    iderw(b);
 }
 
 // Release a locked buffer.
 // Move to the head of the MRU list.
-void
-brelse(struct buf *b)
-{
-  if(!holdingsleep(&b->lock))
-    panic("brelse");
+void brelse(struct buf* b) {
+    if (!holdingsleep(&b->lock))
+        panic("brelse");
 
-  releasesleep(&b->lock);
+    releasesleep(&b->lock);
 
-  acquire(&bcache.lock);
-  b->refcnt--;
-  if (b->refcnt == 0) {
-    // no one is waiting for it.
-    b->next->prev = b->prev;
-    b->prev->next = b->next;
-    b->next = bcache.head.next;
-    b->prev = &bcache.head;
-    bcache.head.next->prev = b;
-    bcache.head.next = b;
-  }
-  
-  release(&bcache.lock);
+    acquire(&bcache.lock);
+    b->refcnt--;
+    if (b->refcnt == 0) {
+        // no one is waiting for it.
+        b->next->prev          = b->prev;
+        b->prev->next          = b->next;
+        b->next                = bcache.head.next;
+        b->prev                = &bcache.head;
+        bcache.head.next->prev = b;
+        bcache.head.next       = b;
+    }
+
+    release(&bcache.lock);
 }
-//PAGEBREAK!
-// Blank page.
-
